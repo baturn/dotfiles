ede0f2d0805c57d15c3f9684f938b0fa1aec4bce
Bryce Turner <85263031+baturn@users.noreply.github.com>
2022-04-18 12:49:59 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ set -xe
 sudo apt-get update
 sudo apt-get -y install zsh-autosuggestions zsh-syntax-highlighting jq ripgrep fzf
 
-cp .zshrc $HOME/.zshrc
+cp /workspaces/.codespaces/.persistedshare/dotfiles/.zshrc $HOME/.zshrc
 sudo chsh -s "$(which zsh)" "$(whoami)"
